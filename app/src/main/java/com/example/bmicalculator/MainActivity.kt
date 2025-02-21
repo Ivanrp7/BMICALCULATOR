174d8aa2c7fc970e90afa15a25a793f83cb8c4f9
ivan <ivanroaprieto@gmail.com>
2025-02-21 12:30:56 +0100

    EMPTY PROJECT

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var heightedittext: EditText
+    lateinit var weightedittext: EditText
+    lateinit var calculatorbutton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
